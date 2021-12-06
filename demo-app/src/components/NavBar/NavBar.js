92cbf428c444655ad294440719ad643ad147fcab
Stella Steinfeld <stella.sblanco@gmail.com>
2021-12-06 13:37:53 -0300

    finished styles and buttons

--- demo-app/src/components/NavBar/NavBar.js
+++ demo-app/src/components/NavBar/NavBar.js
@@ -8,6 +8,8 @@ import {
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faList } from '@fortawesome/free-solid-svg-icons';
 
 import Dashboard from "../../views/Dashboard";
 import AlarmsListing from "../../views/AlarmsListing";
@@ -17,13 +19,13 @@ export default function NavBar() {
     return (
         <Router>
             <>
-                <Navbar bg="light" expand="lg" className="leftNav">
+                <Navbar fixed="top" bg="light" expand="lg" className="leftNav">
                     <Container  className="flex-column">
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto flex-column">
-                                <Link to="/">Dashboard</Link>
-                                <Link to="/alarms">Alarms</Link>
+                                <Link to="/" className="leftNav--buttons btn btn-outline-dark"><FontAwesomeIcon icon={faHome} /></Link>
+                                <Link to="/alarms" className="leftNav--buttons btn btn-outline-dark"><FontAwesomeIcon icon={faList} /></Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
