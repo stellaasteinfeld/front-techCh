37732b74c33d6001614f1732c02f05f502f31ade
Stella Steinfeld <stella.sblanco@gmail.com>
2021-12-06 13:48:30 -0300

    dashboard implementation

--- demo-app/src/views/Dashboard/Dashboard.js
+++ demo-app/src/views/Dashboard/Dashboard.js
@@ -1,5 +1,36 @@
 import React from "react";
+import './Dashboard.scss';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 
 export default function Dashboard() {
-    return <h2>Home</h2>;
+    return (
+        <Container className="dashboard">
+            <Row xs={2}>
+                <Col>
+                    <Card border="secondary" style={{ width: '18rem' }}>
+                        <Card.Header>Alarms</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                n/n Alarms Turned
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card border="secondary" style={{ width: '18rem' }}>
+                        <Card.Header>Other Widget</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                               Just another widget to be implemented in future sprints
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
